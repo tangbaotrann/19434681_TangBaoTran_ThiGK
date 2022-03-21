3f0415270289b1211687f60ad1f01a91f3e38e7a
Bao Tran <baotran2k1@outlook.com>
2022-03-22 06:52:33 +0700

    cau_1: finish

---
+++ app/src/main/java/com/example/tangbaotran_19434681_thigk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tangbaotran_19434681_thigk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
